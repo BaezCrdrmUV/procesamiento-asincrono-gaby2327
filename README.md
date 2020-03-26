8d31378269b9c075e949a11e8e4a626d8ea2c664
gaby2327 <gabriela.uv@outlook.com>
2020-03-25 20:29:06 -0600

    Procesamiento asíncrono
    Generación del programa y README

--- README.md
+++ README.md
@@ -12,8 +12,15 @@
 > Se suele usar para llamadas a sistemas externos, un tipo de operaciones que requieren límites superiores y un código que se debe ejecutar en un momento determinado. Las ventajas clave del procesamiento asíncrono son eficiencia del usuario y adaptabilidad.
   
 - Ejemplo en lenguaje de programación favorito.
-> En este ejemplo, algunas de las instrucciones se ejecutarán a destiempo. Si ejecutamos este ejemplo veremos imprimirse ‘Primero’, ‘Tercero’, ‘Segundo’. Esto porque estamos usando la instrucción setTimeout() que difiere de la ejecución x milisegundos.
+> En este ejemplo, se permite mejorar la reactividad de una aplicación. Para ello utilizaremos la palabra reservada async y await.
+> Si la entrada fuera 100000000 puede tardar bastante el cálculo. Para mejorar el comportamiento se proporciona al usuario terminar la ejecución de la aplicación sin necesidad de ir al administrador de tareas de Windows.
 
 ### Acerca de
 >* Creado en C#
 >* Por: Gabriela Sandoval Cruz
+
+#### Referencias
+* Corzo, G. (2018, 19 junio). Programación Así­ncrona. Recuperado 25 marzo, 2020, de https://medium.com/laboratoria-how-to/programacion-asincrona-cea3bad7c3c6
+* Tutorial sobre programación asíncrona con C# y ejemplos prácticos. (2017, 9 diciembre). Recuperado 25 marzo, 2020, de https://anexsoft.com/tutorial-sobre-programacion-asincrona-con-c-y-ejemplos-practicos
+* Apex así­ncrono. (s.f.). Recuperado 25 marzo, 2020, de https://trailhead.salesforce.com/es-MX/content/learn/modules/asynchronous_apex/async_apex_introduction
+* Grill, T. (s.f.-b). Funciones asíncronas – Tutorial de C#. Recuperado 25 marzo, 2020, de https://csharp.com.es/funciones-asincronas/
---
+++ procesamientoAsincrono/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace procesamientoAsincrono
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string numero;
+            int numeroUsuario;
+
+            Console.WriteLine("--- NUMEROS PRIMOS ---");
+            Console.WriteLine("Ingresa un número: ");
+            numero = Console.ReadLine();
+            numeroUsuario = int.Parse(numero);
+            procesoAsincrono(numeroUsuario);
+            Console.WriteLine("Para salir presiona cualquier tecla.");
+            Console.ReadKey();
+        }
+
+        static async Task procesoAsincrono(int numero)
+        {
+            int resultado;
+            resultado = await cuentaPrimos(numero);
+            Console.WriteLine(resultado);
+            Console.WriteLine("----------------------");
+        }
+
+        public static async Task<int> cuentaPrimos(int numero)
+        {
+            int resultado;
+            resultado = await Task.Run(() =>
+            {
+                int contador;
+                int contadorNumero = 0;
+                {
+                    for(contador = 0; contador <= numero; contador++)
+                    {
+                        if(esPrimo(contador))
+                        {
+                            contadorNumero++;
+                        }
+                    }
+                }
+                return contadorNumero;
+            });
+            return resultado;
+        }
+
+        public static bool esPrimo(int numero)
+        {
+            if(numero < 2)
+            {
+                return false;
+            }
+
+            if(numero == 2)
+            {
+                return true;
+            }
+
+            if(numero % 2 == 0)
+            {
+                return false;
+            }
+
+            int contador = 3;
+
+            while ((contador * contador <= numero))
+            {
+                if (numero % contador == 0)
+                {
+                    return false;
+                }else
+                {
+                    contador++;
+                }
+            }
+            return true;
+        }
+    }
+}
